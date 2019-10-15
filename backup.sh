9c05f1dbf7f7032b5f11e26bba9fa5cefbc34667
CodeForFun-r <56582492+CodeForFun-r@users.noreply.github.com>
2019-10-15 15:24:24 +0530

    Add files via upload
    Edit the file to add the name of your hard disk.

---
+++ backup.sh
@@ -0,0 +1,10 @@
+present=$(lsusb | grep 'First word of your hard disk name')
+if [ -z "$present" ];
+then
+	exit
+else
+     disklabel=$(lsblk --output NAME,VENDOR,MODEL,MOUNTPOINT,LABEL | grep 'First word of your hard disk name' | awk '{print $1}')
+     folderpath=$(lsblk --output NAME,VENDOR,MODEL,MOUNTPOINT,LABEL | grep $disklabel$(echo '1') | grep '/media' | awk '{print $2}')
+	rsync -czvr --progress /home/name/ $folderpath/$(echo 'name')
+fi
+
